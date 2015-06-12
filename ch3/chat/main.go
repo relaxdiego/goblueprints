4f7c084de1c1bed498063f026d4fe1c9c67eb88e
Mark Maglana <mmaglana@gmail.com>
2015-06-12 15:32:20 -0700

    Add support for user avatars
    Conflicts:
    	ch3/chat/auth.go
    	ch3/chat/chat
    	ch3/chat/client.go
    	ch3/chat/main.go
    	ch3/chat/message.go
    	ch3/chat/templates/chat.html

--- ch3/chat/main.go
+++ ch3/chat/main.go
@@ -95,6 +95,17 @@ func main() {
 
 	http.HandleFunc("/auth/", loginHandler)
 
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+
 	// chatroom
 	r := newRoom()
 	// When /room is accessed, the ServeHTTP method of
